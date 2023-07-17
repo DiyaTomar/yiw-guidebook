e0dcd7df86598ea3df225fa1e03724bd04c79de6
Diya Tomar <diyastomar@gmail.com>
2023-07-17 15:22:25 -0400

    Displaying weather in 3-hour intervals

--- src/pages/collegelife/sharedCL/LocationDropdown.tsx
+++ src/pages/collegelife/sharedCL/LocationDropdown.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Select, Option } from '@material-tailwind/react';
 import { useState, useEffect } from 'react';
+import WeatherCard from './WeatherCard';
 
 const apiKey: string | undefined = import.meta.env.VITE_REACT_APP_API_KEY;
 
@@ -10,18 +11,12 @@ export default function LocationDropdown() {
     const [selectedLocation, setSelectedLocation] = useState<
         string | undefined
     >('Wise');
-    // let initialData;
-    // const initialFetch = async () => {
-    //     const url = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&q=${selectedLocation}`;
-    //     const response = await fetch(url);
-    //     initialData = await response.json();
-    // };
-    // initialFetch();
+
     const [weatherData, setWeatherData] = useState<any>(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            const url = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&q=${selectedLocation}&units=imperial`;
+            const url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedLocation}&appid=${apiKey}&units=imperial`;
             const response = await fetch(url);
             setWeatherData(await response.json());
         };
@@ -30,31 +25,35 @@ export default function LocationDropdown() {
     }, [selectedLocation]);
 
     return (
-        <div className="w-72 md:flex">
-            <Select
-                label="Select Location"
-                value="Wise"
-                onChange={(event) => {
-                    setSelectedLocation(event);
-                }}
-                variant="outlined"
-                color="blue"
-                animate={{
-                    mount: { y: 0 },
-                    unmount: { y: 25 },
-                }}
-            >
-                {locations.map((location) => (
-                    <Option key={location} value={location}>
-                        {location}
-                    </Option>
-                ))}
-            </Select>
-            <div>
+        <div className="md:flex gap-">
+            <div className="w-64 sm:mb-16">
+                <Select
+                    label="Select Location"
+                    value="Wise"
+                    onChange={(event) => {
+                        setSelectedLocation(event);
+                    }}
+                    variant="outlined"
+                    color="blue"
+                    animate={{
+                        mount: { y: 0 },
+                        unmount: { y: 25 },
+                    }}
+                >
+                    {locations.map((location) => (
+                        <Option key={location} value={location}>
+                            {location}
+                        </Option>
+                    ))}
+                </Select>
+            </div>
+            <div className="">
                 {weatherData && weatherData.list && (
-                    <ul>
+                    <ul className="flex">
                         {weatherData.list.slice(0, 5).map((day: any) => (
-                            <li key={day.dt}> Temperature: {day.main.temp} </li>
+                            <li key={day.dt}>
+                                <WeatherCard day={day} />
+                            </li>
                         ))}
                     </ul>
                 )}
---
+++ src/pages/collegelife/sharedCL/WeatherCard.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type Props = {
+    day: any;
+};
+
+function WeatherCard({ day }: Props) {
+    const temperature = Math.round(day.main.temp);
+
+    const date = new Date(day.dt * 1000).toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+    });
+
+    return (
+        <div className="h-40 w-40 border-2 text-center ">
+            <div>{date}</div>
+            <div className="flex justify-center h-16">
+                <img
+                    src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                    alt="Weather Icon"
+                />
+            </div>
+            <div className="text-lg font-bold">{temperature} °F</div>
+            <div className="text-sm">{day.weather[0].main}</div>
+            <div className="text-xs">{day.weather[0].description}</div>
+        </div>
+    );
+}
+
+export default WeatherCard;
